eebd101a21e751d8980389d014f7215157ad43d7
Sergey228i666i69 <cgenerozov@gmail.com>
2026-01-07 03:54:34 +0700

    Перед работой над 5 циклом

--- scripts/objects/trigger_set_property.gd
+++ scripts/objects/trigger_set_property.gd
@@ -12,6 +12,13 @@ extends Area2D
 @export var property_name: String = ""
 @export var value: Variant
 
+@export_group("Sound")
+@export var sfx_stream: AudioStream
+@export var sfx_position: Vector2 = Vector2.ZERO
+@export var sfx_use_trigger_position: bool = false
+@export var sfx_delay: float = 0.0
+@export var sfx_volume_db: float = 0.0
+
 var _has_fired: bool = false
 
 func _ready() -> void:
@@ -53,7 +60,24 @@ func _apply() -> void:
 			if not _has_property(node, property_name):
 				continue
 			node.set(property_name, value)
+	_play_sound()
 	_has_fired = true
+
+func _play_sound() -> void:
+	if sfx_stream == null:
+		return
+	var play := func() -> void:
+		var player := AudioStreamPlayer2D.new()
+		player.stream = sfx_stream
+		player.volume_db = sfx_volume_db
+		player.global_position = global_position if sfx_use_trigger_position else sfx_position
+		player.finished.connect(player.queue_free)
+		get_tree().current_scene.add_child(player)
+		player.play()
+	if sfx_delay > 0.0:
+		get_tree().create_timer(sfx_delay).timeout.connect(play)
+	else:
+		play.call()
 
 func _apply_change(change: PropertyChange) -> void:
 	if change == null:
